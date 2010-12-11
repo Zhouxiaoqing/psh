ed308a4bc4fc751601e1b1aec3f80ae5d78c4f07
Taku Fukushima <f.tac.mac@gmail.com>
2010-12-12 00:46:15 +0900

    [Refactor] add under score to static functions

--- parser.c
+++ parser.c
@@ -17,17 +17,17 @@
 
 #include "parser.h"
 
-static const token_t *parse_word(tokenizer_t *t);
-static const token_t *parse_env(tokenizer_t *t);
-static const token_t *parse_env_assignment(tokenizer_t *t);
-static const token_t *parse_home(tokenizer_t *t);
-static const token_t *parse_redirect_out(tokenizer_t *t);
-static const token_t *parse_redirect_in(tokenizer_t *t);
-static const token_t *parse_redirection(tokenizer_t *t);
-static const token_t *parse_command_element(tokenizer_t *t);
-static const token_t *parse_redirection_list(tokenizer_t *t);
-static const token_t *parse_command(tokenizer_t *t);
-static const token_t *parse_piped_command(tokenizer_t *t);
+static const token_t *_parse_word(tokenizer_t *t);
+static const token_t *_parse_env(tokenizer_t *t);
+static const token_t *_parse_env_assignment(tokenizer_t *t);
+static const token_t *_parse_home(tokenizer_t *t);
+static const token_t *_parse_redirect_out(tokenizer_t *t);
+static const token_t *_parse_redirect_in(tokenizer_t *t);
+static const token_t *_parse_redirection(tokenizer_t *t);
+static const token_t *_parse_command_element(tokenizer_t *t);
+static const token_t *_parse_redirection_list(tokenizer_t *t);
+static const token_t *_parse_command(tokenizer_t *t);
+static const token_t *_parse_piped_command(tokenizer_t *t);
 
 /**
  * syntax_error - Deal with syntax error of input.
@@ -41,9 +41,9 @@ void syntax_error(tokenizer_t *t)
 }
 
 /*
- * parse_word - Eat <word>
+ * _parse_word - Eat <word>
  */
-static const token_t *parse_word(tokenizer_t *t)
+static const token_t *_parse_word(tokenizer_t *t)
 {
     const token_t *word;
     command_t *current_command;
@@ -64,9 +64,9 @@ static const token_t *parse_word(tokenizer_t *t)
 }
 
 /*
- * parse_env - Eat <env>
+ * _parse_env - Eat <env>
  */
-static const token_t *parse_env(tokenizer_t *t)
+static const token_t *_parse_env(tokenizer_t *t)
 {
     const token_t *doller, *env;
 
@@ -75,13 +75,13 @@ static const token_t *parse_env(tokenizer_t *t)
     env = next_token(t);
     _next_token(t);
 
-    return parse_word(t);
+    return _parse_word(t);
 }
 
 /*
- * parse_env_assignment - Eat <env_assignment>
+ * _parse_env_assignment - Eat <env_assignment>
  */
-static const token_t *parse_env_assignment(tokenizer_t *t)
+static const token_t *_parse_env_assignment(tokenizer_t *t)
 {
     const token_t *key, *assign, *value;
     
@@ -100,9 +100,9 @@ static const token_t *parse_env_assignment(tokenizer_t *t)
 }
 
 /*
- * parse_redirect_in - Eat <redirect_in>
+ * _parse_redirect_in - Eat <redirect_in>
  */
-static const token_t *parse_redirect_in(tokenizer_t *t)
+static const token_t *_parse_redirect_in(tokenizer_t *t)
 {
     const token_t *redirect_head;
     const token_t *redirect_file;
@@ -116,7 +116,7 @@ static const token_t *parse_redirect_in(tokenizer_t *t)
         token_t *redirect_next = next_token(t);
     }
     if (redirect_next->spec == REDIRECT_IN_APPEND) {
-        token_t *word = parse_redirect_in_append(t);
+        token_t *word = _parse_redirect_in_append(t);
     }
     */
     if (redirect_head->spec != REDIRECT_IN) syntax_error(t);
@@ -127,9 +127,9 @@ static const token_t *parse_redirect_in(tokenizer_t *t)
 }
 
 /*
- * parse_redirect_out - Eat <redirect_out>
+ * _parse_redirect_out - Eat <redirect_out>
  */
-static const token_t *parse_home(tokenizer_t *t)
+static const token_t *_parse_home(tokenizer_t *t)
 {
     const token_t *home;
     
@@ -141,9 +141,9 @@ static const token_t *parse_home(tokenizer_t *t)
 }
 
 /*
- * parse_redirect_out - Eat <redirect_out>
+ * _parse_redirect_out - Eat <redirect_out>
  */
-static const token_t *parse_redirect_out(tokenizer_t *t)
+static const token_t *_parse_redirect_out(tokenizer_t *t)
 {
     const token_t *redirect_head;
     const token_t *redirect_file;
@@ -157,7 +157,7 @@ static const token_t *parse_redirect_out(tokenizer_t *t)
         token_t *redirect_next = next_token(t);
     }
     if (redirect_next->spec == REDIRECT_OUT_APPEND) {
-        token_t *word = parse_redirect_in_append(t);
+        token_t *word = _parse_redirect_in_append(t);
     }
     */
     if (redirect_head->spec != REDIRECT_OUT) syntax_error(t);
@@ -168,9 +168,9 @@ static const token_t *parse_redirect_out(tokenizer_t *t)
 }
 
 /*
- * parse_redirection - Eat <redirection>
+ * _parse_redirection - Eat <redirection>
  */
-static const token_t *parse_redirection(tokenizer_t *t)
+static const token_t *_parse_redirection(tokenizer_t *t)
 {
     const token_t *redirect_head;
 
@@ -180,19 +180,19 @@ static const token_t *parse_redirection(tokenizer_t *t)
         syntax_error(t);
     switch (redirect_head->spec) {
     case REDIRECT_IN:
-        parse_redirect_in(t);
+        _parse_redirect_in(t);
         break;
     case REDIRECT_OUT:
-        parse_redirect_out(t);
+        _parse_redirect_out(t);
         break;
     }
     return next_token(t);
 }
 
 /*
- * parse_redirection_list - Eat <redirection_list>
+ * _parse_redirection_list - Eat <redirection_list>
  */
-static const token_t *parse_redirection_list(tokenizer_t *t)
+static const token_t *_parse_redirection_list(tokenizer_t *t)
 {
     const token_t *redirection;
     const token_t *redirection_list;
@@ -201,32 +201,32 @@ static const token_t *parse_redirection_list(tokenizer_t *t)
     if (redirection->spec != REDIRECT_IN &&
         redirection->spec != REDIRECT_OUT)
         syntax_error(t);
-    parse_redirection(t);
+    _parse_redirection(t);
     redirection_list = next_token(t);
     if (redirection->spec == REDIRECT_IN &&
         redirection->spec == REDIRECT_OUT)
-        parse_redirection_list(t);
+        _parse_redirection_list(t);
 
     return redirection_list;
 }
 
 /*
- * parse_command_element - Eat <command_element>
+ * _parse_command_element - Eat <command_element>
  */
-static const token_t *parse_command_element(tokenizer_t *t)
+static const token_t *_parse_command_element(tokenizer_t *t)
 {
     const token_t *command_element;
     
     command_element = current_token(t);
     switch (command_element->spec) {
     case WORD:
-        parse_word(t);
+        _parse_word(t);
         break;
     case ENV_ASSIGNMENT:
-        parse_env_assignment(t);
+        _parse_env_assignment(t);
         break;
     case REDIRECT_IN: case REDIRECT_OUT:
-        parse_redirection_list(t);
+        _parse_redirection_list(t);
         break;
     default: break;
     }
@@ -235,9 +235,9 @@ static const token_t *parse_command_element(tokenizer_t *t)
 }
 
 /*
- * parse_command - Eat <command> and make pipe.
+ * _parse_command - Eat <command> and make pipe.
  */
-static const token_t *parse_command(tokenizer_t *t)
+static const token_t *_parse_command(tokenizer_t *t)
 {
     const token_t *command_element;
     const token_t *command;
@@ -248,28 +248,28 @@ static const token_t *parse_command(tokenizer_t *t)
         command_element->spec != REDIRECT_IN &&
         command_element->spec != REDIRECT_OUT)
         syntax_error(t);
-    parse_command_element(t);
+    _parse_command_element(t);
     command = next_token(t);
     if (command->spec == WORD ||
         command->spec == ENV_ASSIGNMENT ||
         command->spec == REDIRECT_IN ||
         command->spec == REDIRECT_OUT)
-        parse_command(t);
+        _parse_command(t);
     
     return command;
 }
 
 /*
- * parse_piped_command - Eat <piped_command>
+ * _parse_piped_command - Eat <piped_command>
  */
-static const token_t *parse_piped_command(tokenizer_t *t)
+static const token_t *_parse_piped_command(tokenizer_t *t)
 {
     const token_t *command;
     const token_t *pipe;
 
     command = current_token(t);
     // Eat command and bind it to 
-    pipe = parse_command(t);
+    pipe = _parse_command(t);
     if (t->p <= PIPE_MAX) {
         t->p++;
     } else {
@@ -279,7 +279,7 @@ static const token_t *parse_piped_command(tokenizer_t *t)
 
     if (pipe->spec == PIPED_COMMAND) {
         next_token(t);
-        parse_piped_command(t);
+        _parse_piped_command(t);
     }
    
     return command;
@@ -291,5 +291,5 @@ static const token_t *parse_piped_command(tokenizer_t *t)
  */
 const token_t *parse_input(tokenizer_t *t)
 {
-    return parse_piped_command(t);
+    return _parse_piped_command(t);
 }
